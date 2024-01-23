1720c310d8808729a21b58e9773dd93cf8930d19
saminoorsyed <saminoorsyed@gmail.com>
2024-01-23 16:46:25 -0500

    begin writing dissasembler function

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
-# EmulatorNotes
-A place to keep personal notes for a group emulator project
+# Emulator8080
+
+Description: This Repository is focused on building an Emulator for the 8080 processor with a group of 4 fellow students over the course of 3 months. Our eventual goal is to run the Space invaders game on it. As a side effect of the above goal, I hope to further my knowledge of the C++ and C programming languages as well as Assembly Language and the low level programming that is involved with operating computer systems.
+
+## Project outline and order of goals:
+
+The following will be the basic order in which our team tackles building our project:
+
+-Set up environment and ensure team members can successfully compile code with requisite basic libraries
+-Build a disassembler for the Space Invaders ROM.
+    -Based on 8080 opcode
+    -Document disassembled code for human readability
+-Build an Emulator File for the 8080 Processor.
+    -Write emulator shell
+    -Implement code for basic logic groups
+    -Implement instructions required for 8080 assembly code
+    -Debug, debug, debug
+-Build an emulator file for the hardware of the arcade processor.
+    -Visual interrupts
+    -Processor timing
+    -Audio outputs
+    -Keyboard inputs
+    -Debug
+-Build a file for interpreting i/o from our computers
+    -Keyboard inputs
+    -Audio outputs
+    -Platform-specific instructions/code
+    -Debug
+-Build a GUI to interact with the Emulator
+    -Start game option
+    -Exit game option
+-(Optional) Add additional games for emulation
+    -Implement additional assembly instructions
+    -Add game-selection for GUI
+    -Debug and ensure that hardware i/o is consistent
+-Play the Game
+
+## Set up environment and ensure team members can successfully compile code with requisite basic libraries
+
+- We will be using C++ for the majority of our code.
+- We will also use some assembly language for our project. 
+- Over the course of our project, we will conduct research and test several libraries for building GUIs. We have done some preliminary research into libraries like GTK and Qt, but have not yet made a decision on which library to use.
+
+
+## Build a disassembler for the Space Invaders ROM.
+
+
+### tokens.cpp
+This file will define the appropriate output given an input using the op codes provided in the 8080
+
+## Build an Emulator File for the 8080 Processor.
+
+
+## Build an emulator file for the hardware of the arcade processor.
+
+
+## Build a file for interpreting i/o from our computers
+
+
+## Build a GUI to interact with the Emulator
+
--- disassembler/disassembler.cpp
+++ disassembler/disassembler.cpp
@@ -1,6 +1,5 @@
-// Author: Sami Syed
 // Date: Jan 22, 2024
-// Purpose: Dissasembler for Space invaders
+// Purpose: DisasSembler for Space invaders
 
 #include <iostream>
 using namespace std;
@@ -43,6 +42,748 @@ int Disassemble8080Op(unsigned char *codebuffer, int pc)
     case 0x08:
         printf("NOP");
         break;
+    case 0x09:
+        printf();
+        break;
+    case 0x0A:
+        printf();
+        break;
+    case 0x0B:
+        printf();
+        break;
+    case 0x0C:
+        printf();
+        break;
+    case 0x0D:
+        printf();
+        break;
+    case 0x0E:
+        printf();
+        break;
+    case 0x0F:
+        printf();
+        break;
+    case 0x10:
+        printf();
+        break;
+    case 0x11:
+        printf();
+        break;
+    case 0x12:
+        printf();
+        break;
+    case 0x13:
+        printf();
+        break;
+    case 0x14:
+        printf();
+        break;
+    case 0x15:
+        printf();
+        break;
+    case 0x16:
+        printf();
+        break;
+    case 0x17:
+        printf();
+        break;
+    case 0x18:
+        printf();
+        break;
+    case 0x19:
+        printf();
+        break;
+    case 0x1A:
+        printf();
+        break;
+    case 0x1B:
+        printf();
+        break;
+    case 0x1C:
+        printf();
+        break;
+    case 0x1D:
+        printf();
+        break;
+    case 0x1E:
+        printf();
+        break;
+    case 0x1F:
+        printf();
+        break;
+    case 0x20:
+        printf();
+        break;
+    case 0x21:
+        printf();
+        break;
+    case 0x22:
+        printf();
+        break;
+    case 0x23:
+        printf();
+        break;
+    case 0x24:
+        printf();
+        break;
+    case 0x25:
+        printf();
+        break;
+    case 0x26:
+        printf();
+        break;
+    case 0x27:
+        printf();
+        break;
+    case 0x28:
+        printf();
+        break;
+    case 0x29:
+        printf();
+        break;
+    case 0x2A:
+        printf();
+        break;
+    case 0x2B:
+        printf();
+        break;
+    case 0x2C:
+        printf();
+        break;
+    case 0x2D:
+        printf();
+        break;
+    case 0x2E:
+        printf();
+        break;
+    case 0x2F:
+        printf();
+        break;
+    case 0x30:
+        printf();
+        break;
+    case 0x31:
+        printf();
+        break;
+    case 0x32:
+        printf();
+        break;
+    case 0x33:
+        printf();
+        break;
+    case 0x34:
+        printf();
+        break;
+    case 0x35:
+        printf();
+        break;
+    case 0x36:
+        printf();
+        break;
+    case 0x37:
+        printf();
+        break;
+    case 0x38:
+        printf();
+        break;
+    case 0x39:
+        printf();
+        break;
+    case 0x3A:
+        printf();
+        break;
+    case 0x3B:
+        printf();
+        break;
+    case 0x3C:
+        printf();
+        break;
+    case 0x3D:
+        printf();
+        break;
+    case 0x3E:
+        printf();
+        break;
+    case 0x3F:
+        printf();
+        break;
+    case 0x40:
+        printf();
+        break;
+    case 0x41:
+        printf();
+        break;
+    case 0x42:
+        printf();
+        break;
+    case 0x43:
+        printf();
+        break;
+    case 0x44:
+        printf();
+        break;
+    case 0x45:
+        printf();
+        break;
+    case 0x46:
+        printf();
+        break;
+    case 0x47:
+        printf();
+        break;
+    case 0x48:
+        printf();
+        break;
+    case 0x49:
+        printf();
+        break;
+    case 0x4A:
+        printf();
+        break;
+    case 0x4B:
+        printf();
+        break;
+    case 0x4C:
+        printf();
+        break;
+    case 0x4D:
+        printf();
+        break;
+    case 0x4E:
+        printf();
+        break;
+    case 0x4F:
+        printf();
+        break;
+    case 0x50:
+        printf();
+        break;
+    case 0x51:
+        printf();
+        break;
+    case 0x52:
+        printf();
+        break;
+    case 0x53:
+        printf();
+        break;
+    case 0x54:
+        printf();
+        break;
+    case 0x55:
+        printf();
+        break;
+    case 0x56:
+        printf();
+        break;
+    case 0x57:
+        printf();
+        break;
+    case 0x58:
+        printf();
+        break;
+    case 0x59:
+        printf();
+        break;
+    case 0x5A:
+        printf();
+        break;
+    case 0x5B:
+        printf();
+        break;
+    case 0x5C:
+        printf();
+        break;
+    case 0x5D:
+        printf();
+        break;
+    case 0x5E:
+        printf();
+        break;
+    case 0x5F:
+        printf();
+        break;
+    case 0x60:
+        printf();
+        break;
+    case 0x61:
+        printf();
+        break;
+    case 0x62:
+        printf();
+        break;
+    case 0x63:
+        printf();
+        break;
+    case 0x64:
+        printf();
+        break;
+    case 0x65:
+        printf();
+        break;
+    case 0x66:
+        printf();
+        break;
+    case 0x67:
+        printf();
+        break;
+    case 0x68:
+        printf();
+        break;
+    case 0x69:
+        printf();
+        break;
+    case 0x6A:
+        printf();
+        break;
+    case 0x6B:
+        printf();
+        break;
+    case 0x6C:
+        printf();
+        break;
+    case 0x6D:
+        printf();
+        break;
+    case 0x6E:
+        printf();
+        break;
+    case 0x6F:
+        printf();
+        break;
+    case 0x70:
+        printf();
+        break;
+    case 0x71:
+        printf();
+        break;
+    case 0x72:
+        printf();
+        break;
+    case 0x73:
+        printf();
+        break;
+    case 0x74:
+        printf();
+        break;
+    case 0x75:
+        printf();
+        break;
+    case 0x76:
+        printf();
+        break;
+    case 0x77:
+        printf();
+        break;
+    case 0x78:
+        printf();
+        break;
+    case 0x79:
+        printf();
+        break;
+    case 0x7A:
+        printf();
+        break;
+    case 0x7B:
+        printf();
+        break;
+    case 0x7C:
+        printf();
+        break;
+    case 0x7D:
+        printf();
+        break;
+    case 0x7E:
+        printf();
+        break;
+    case 0x7F:
+        printf();
+        break;
+    case 0x80:
+        printf();
+        break;
+    case 0x81:
+        printf();
+        break;
+    case 0x82:
+        printf();
+        break;
+    case 0x83:
+        printf();
+        break;
+    case 0x84:
+        printf();
+        break;
+    case 0x85:
+        printf();
+        break;
+    case 0x86:
+        printf();
+        break;
+    case 0x87:
+        printf();
+        break;
+    case 0x88:
+        printf();
+        break;
+    case 0x89:
+        printf();
+        break;
+    case 0x8A:
+        printf();
+        break;
+    case 0x8B:
+        printf();
+        break;
+    case 0x8C:
+        printf();
+        break;
+    case 0x8D:
+        printf();
+        break;
+    case 0x8E:
+        printf();
+        break;
+    case 0x8F:
+        printf();
+        break;
+    case 0x90:
+        printf();
+        break;
+    case 0x91:
+        printf();
+        break;
+    case 0x92:
+        printf();
+        break;
+    case 0x93:
+        printf();
+        break;
+    case 0x94:
+        printf();
+        break;
+    case 0x95:
+        printf();
+        break;
+    case 0x96:
+        printf();
+        break;
+    case 0x97:
+        printf();
+        break;
+    case 0x98:
+        printf();
+        break;
+    case 0x99:
+        printf();
+        break;
+    case 0x9A:
+        printf();
+        break;
+    case 0x9B:
+        printf();
+        break;
+    case 0x9C:
+        printf();
+        break;
+    case 0x9D:
+        printf();
+        break;
+    case 0x9E:
+        printf();
+        break;
+    case 0x9F:
+        printf();
+        break;
+    case 0xA0:
+        printf();
+        break;
+    case 0xA1:
+        printf();
+        break;
+    case 0xA2:
+        printf();
+        break;
+    case 0xA3:
+        printf();
+        break;
+    case 0xA4:
+        printf();
+        break;
+    case 0xA5:
+        printf();
+        break;
+    case 0xA6:
+        printf();
+        break;
+    case 0xA7:
+        printf();
+        break;
+    case 0xA8:
+        printf();
+        break;
+    case 0xA9:
+        printf();
+        break;
+    case 0xAA:
+        printf();
+        break;
+    case 0xAB:
+        printf();
+        break;
+    case 0xAC:
+        printf();
+        break;
+    case 0xAD:
+        printf();
+        break;
+    case 0xAE:
+        printf();
+        break;
+    case 0xAF:
+        printf();
+        break;
+    case 0xB0:
+        printf();
+        break;
+    case 0xB1:
+        printf();
+        break;
+    case 0xB2:
+        printf();
+        break;
+    case 0xB3:
+        printf();
+        break;
+    case 0xB4:
+        printf();
+        break;
+    case 0xB5:
+        printf();
+        break;
+    case 0xB6:
+        printf();
+        break;
+    case 0xB7:
+        printf();
+        break;
+    case 0xB8:
+        printf();
+        break;
+    case 0xB9:
+        printf();
+        break;
+    case 0xBA:
+        printf();
+        break;
+    case 0xBB:
+        printf();
+        break;
+    case 0xBC:
+        printf();
+        break;
+    case 0xBD:
+        printf();
+        break;
+    case 0xBE:
+        printf();
+        break;
+    case 0xBF:
+        printf();
+        break;
+    case 0xC0:
+        printf();
+        break;
+    case 0xC1:
+        printf();
+        break;
+    case 0xC2:
+        printf();
+        break;
+    case 0xC3:
+        printf();
+        break;
+    case 0xC4:
+        printf();
+        break;
+    case 0xC5:
+        printf();
+        break;
+    case 0xC6:
+        printf();
+        break;
+    case 0xC7:
+        printf();
+        break;
+    case 0xC8:
+        printf();
+        break;
+    case 0xC9:
+        printf();
+        break;
+    case 0xCA:
+        printf();
+        break;
+    case 0xCB:
+        printf();
+        break;
+    case 0xCC:
+        printf();
+        break;
+    case 0xCD:
+        printf();
+        break;
+    case 0xCE:
+        printf();
+        break;
+    case 0xCF:
+        printf();
+        break;
+    case 0xD0:
+        printf();
+        break;
+    case 0xD1:
+        printf();
+        break;
+    case 0xD2:
+        printf();
+        break;
+    case 0xD3:
+        printf();
+        break;
+    case 0xD4:
+        printf();
+        break;
+    case 0xD5:
+        printf();
+        break;
+    case 0xD6:
+        printf();
+        break;
+    case 0xD7:
+        printf();
+        break;
+    case 0xD8:
+        printf();
+        break;
+    case 0xD9:
+        printf();
+        break;
+    case 0xDA:
+        printf();
+        break;
+    case 0xDB:
+        printf();
+        break;
+    case 0xDC:
+        printf();
+        break;
+    case 0xDD:
+        printf();
+        break;
+    case 0xDE:
+        printf();
+        break;
+    case 0xDF:
+        printf();
+        break;
+    case 0xE0:
+        printf();
+        break;
+    case 0xE1:
+        printf();
+        break;
+    case 0xE2:
+        printf();
+        break;
+    case 0xE3:
+        printf();
+        break;
+    case 0xE4:
+        printf();
+        break;
+    case 0xE5:
+        printf();
+        break;
+    case 0xE6:
+        printf();
+        break;
+    case 0xE7:
+        printf();
+        break;
+    case 0xE8:
+        printf();
+        break;
+    case 0xE9:
+        printf();
+        break;
+    case 0xEA:
+        printf();
+        break;
+    case 0xEB:
+        printf();
+        break;
+    case 0xEC:
+        printf();
+        break;
+    case 0xED:
+        printf();
+        break;
+    case 0xEE:
+        printf();
+        break;
+    case 0xEF:
+        printf();
+        break;
+    case 0xF0:
+        printf();
+        break;
+    case 0xF1:
+        printf();
+        break;
+    case 0xF2:
+        printf();
+        break;
+    case 0xF3:
+        printf();
+        break;
+    case 0xF4:
+        printf();
+        break;
+    case 0xF5:
+        printf();
+        break;
+    case 0xF6:
+        printf();
+        break;
+    case 0xF7:
+        printf();
+        break;
+    case 0xF8:
+        printf();
+        break;
+    case 0xF9:
+        printf();
+        break;
+    case 0xFA:
+        printf();
+        break;
+    case 0xFB:
+        printf();
+        break;
+    case 0xFC:
+        printf();
+        break;
+    case 0xFD:
+        printf();
+        break;
+    case 0xFE:
+        printf();
+        break;
+    case 0xFF:
+        printf();
+        break;
+
     /* ........ */
     case 0x3e:
         printf("MVI    A,#0x%02x", code[1]);
