4d24a6290ac380079d8c35daedea542b30fc5b2f
Aidan MacKenzie <mackenai@oregonstate.edu>
2024-02-09 10:37:38 -0500

    Update to correct AC flag handling for: 0x14 0x15 0x1C 0x1D 0x24 0x25

--- emulator/emulator_shell.cpp
+++ emulator/emulator_shell.cpp
@@ -198,7 +198,7 @@ int CPU::Emulate8080Codes(State8080 *state){
             state->f.z = state->d == 0;
             state->f.s = 0x80 == (state->d & 0x80);
             state->f.p = Parity(state->d);
-            state->f.ac = 1;
+            state->f.ac = (state->d & 0x0F) == 0x00; //Sets AC if lower nibble overflows
             break;
 
         case 0x15: //DCR D
@@ -206,7 +206,7 @@ int CPU::Emulate8080Codes(State8080 *state){
             state->f.z = state->d == 0;
             state->f.s = 0x80 == (state->d & 0x80);
             state->f.p = Parity(state->d);
-            state->f.ac = 1;
+            state->f.ac = (state->d & 0x0F) == 0x0F; //Sets AC if lower nibble has to borrow from bit 4
             break;
 
         case 0x16: //MVI D, d8
@@ -258,7 +258,7 @@ int CPU::Emulate8080Codes(State8080 *state){
             state->f.z = state->e == 0;
             state->f.s = 0x80 == (state->e & 0x80);
             state->f.p = Parity(state->e);
-            state->f.ac = 1;
+            state->f.ac = (state->d & 0x0F) == 0x00; //Sets AC if lower nibble overflows
             break;
 
         case 0x1D: //DCR E
@@ -266,7 +266,7 @@ int CPU::Emulate8080Codes(State8080 *state){
             state->f.z = state->e == 0;
             state->f.s = 0x80 == (state->e & 0x80);
             state->f.p = Parity(state->e);
-            state->f.ac = 1;
+            state->f.ac = (state->e & 0x0F) == 0x0F; //Sets AC if lower nibble has to borrow from bit 4
             break;
 
         case 0x1E: //MVI E, d8
@@ -314,7 +314,7 @@ int CPU::Emulate8080Codes(State8080 *state){
             state->f.z = state->h == 0;
             state->f.s = 0x80 == (state->h & 0x80);
             state->f.p = Parity(state->h);
-            state->f.ac = 1;
+            state->f.ac = (state->h & 0x0F) == 0x00; //Sets AC if lower nibble overflows
             break;
 
         case 0x25: //DCR H
@@ -322,7 +322,7 @@ int CPU::Emulate8080Codes(State8080 *state){
             state->f.z = state->h == 0;
             state->f.s = 0x80 == (state->h & 0x80);
             state->f.p = Parity(state->h);
-            state->f.ac = 1;
+            state->f.ac = (state->e & 0x0F) == 0x0F; //Sets AC if lower nibble has to borrow from bit 4
             break;
 
         case 0x26: //MVI H, d8
