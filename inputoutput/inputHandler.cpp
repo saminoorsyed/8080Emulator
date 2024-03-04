531da2cacc9c70a0661a5da7e04baa70d0e92076
saminoorsyed <saminoorsyed@gmail.com>
2024-03-04 10:09:47 -0500

    basics of IO using POLL events

---
+++ inputoutput/inputHandler.cpp
@@ -0,0 +1,73 @@
+#include "inputHandler.h"
+#include <SDL2/SDL.h>
+#include <iostream>
+
+void LoadPorts(CPU::State8080 *state, SDL_Event event)
+{
+    if (event.type == SDL_KEYDOWN)
+    {
+        switch (event.key.keysym.sym)
+        {
+        case SDLK_c: // coin
+            state->port1 |= 1;
+            break;
+        case SDLK_g: // p2 start
+            state->port1 |= 1 << 1;
+            break;
+        case SDLK_h: // p1 start
+            state->port1 |= 1 << 2;
+            break;
+        case SDLK_i: // p1 shoot
+            state->port1 |= 1 < 4;
+            break;
+        case SDLK_j: // p1 left
+            state->port1 |= 1 << 5;
+            break;
+        case SDLK_l: // p1 right
+            state->port1 |= 1 << 6;
+            break;
+        case SDLK_e: // p2 shoot
+            state->port2 |= 1 << 4;
+            break;
+        case SDLK_s: // p2 left
+            state->port2 |= 1 << 5;
+            break;
+        case SDLK_f: // p2 right
+            state->port2 |= 1 << 6;
+            break;
+        }
+    }
+    else if (event.type == SDL_KEYUP)
+    {
+        switch (event.key.keysym.sym)
+        {
+        case SDLK_c: // coin
+            state->port1 &= ~1;
+            break;
+        case SDLK_g: // p2 start
+            state->port1 &= ~(1 << 1);
+            break;
+        case SDLK_h: // p1 start
+            state->port1 &= ~(1 << 2);
+            break;
+        case SDLK_i: // p1 shoot
+            state->port1 &= ~(1 < 4);
+            break;
+        case SDLK_j: // p1 left
+            state->port1 &= ~(1 << 5);
+            break;
+        case SDLK_l: // p1 right
+            state->port1 &= ~(1 << 6);
+            break;
+        case SDLK_e: // p2 shoot
+            state->port2 &= ~(1 << 4);
+            break;
+        case SDLK_s: // p2 left
+            state->port2 &= ~(1 << 5);
+            break;
+        case SDLK_f: // p2 right
+            state->port2 &= ~(1 << 6);
+            break;
+        }
+    }
+}
---
+++ inputoutput/inputHandler.h
@@ -0,0 +1,5 @@
+#include <cstdint>
+#include "../emulator/emulator_shell.h"
+#include <SDL2/SDL.h>
+
+void LoadPorts(CPU::State8080* state, SDL_Event event);
